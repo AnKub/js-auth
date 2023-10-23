5f5ff45d04fb3b3f349f8227450eb37062a4c584
AnKub <ankub3088@gmail.com>
2023-10-23 20:02:03 +0300

    almoustDone

--- src/class/user.js
+++ src/class/user.js
@@ -9,9 +9,25 @@ class User {
   constructor({ email, password, role }) {
     this.email = email
     this.password = password
-    this.role = role
+    this.role = User.#convertRole(role)
   }
-  static #coverRole = (role) => {}
+  static #convertRole = (role) => {
+    role = Number(role)
+    if (isNaN(role)) {
+      role = this.USER_ROLE.USER
+    }
+    role = Object.values(this.USER_ROLE).includes(role)
+      ? role
+      : this.USER_ROLE.USER
+
+    return role
+  }
+
+  static create(data) {
+    const user = new User(data)
+
+    this.#list.push(user)
+  }
 }
 
 module.exports = {
---
+++ src/component/field-select/index.js
@@ -0,0 +1,8 @@
+class FieldSelect {
+  static toggle = (target) => {
+    const options = target.nextElementSibling
+
+    options.toggleAttribute('active')
+  }
+}
+window.fieldSelect = FieldSelect
