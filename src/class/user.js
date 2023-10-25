fd07b7bbe02bd015ef651d81396264bba97e4014
AnKub <ankub3088@gmail.com>
2023-10-25 18:43:24 +0300

    third part of authentication

--- src/class/user.js
+++ src/class/user.js
@@ -5,11 +5,13 @@ class User {
     DEVELOPER: 3,
   }
   static #list = []
-
+  static #count = 1
   constructor({ email, password, role }) {
-    this.email = email
-    this.password = password
+    this.id = User.#count++
+    this.email = String(email).toLowerCase()
+    this.password = String(password)
     this.role = User.#convertRole(role)
+    this.isConfirm = false
   }
   static #convertRole = (role) => {
     role = Number(role)
@@ -19,24 +21,32 @@ class User {
     role = Object.values(this.USER_ROLE).includes(role)
       ? role
       : this.USER_ROLE.USER
-
     return role
   }
-
   static create(data) {
     const user = new User(data)
-
+    console.log(user)
     this.#list.push(user)
-
     console.log(this.#list)
+    return user
   }
-
   static getByEmail(email) {
     return (
-      this.#list.find((user) => user.email === email) ||
+      this.#list.find(
+        (user) =>
+          user.email === String(email).toLowerCase(),
+      ) || null
+    )
+  }
+
+  static getById(id) {
+    return (
+      this.#list.find((user) => user.id === Number(id)) ||
       null
     )
   }
+
+  static getList = () => this.#list
 }
 
 module.exports = {
